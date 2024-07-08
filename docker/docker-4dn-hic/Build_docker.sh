b6fcee1750572a545bf326520dd37b7ae1bbb835
Hisashi Miura <30642262+kuzobuta@users.noreply.github.com>
2024-07-09 08:56:42 +0900

    Update Build_docker.sh

--- docker/docker-4dn-hic/Build_docker.sh
+++ docker/docker-4dn-hic/Build_docker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #240708 New docker images
+wget https://repo.anaconda.com/miniconda/Miniconda2-4.6.14-Linux-x86_64.sh
 docker build -t kuzobuta/4dn-hic:v43.1 .
+rm Miniconda2-4.6.14-Linux-x86_64.sh
