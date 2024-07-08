4c09649247b9896ee498664e50f5bb3b6237fd46
Hisashi Miura <30642262+kuzobuta@users.noreply.github.com>
2024-07-09 08:40:11 +0900

    Update Build_docker.sh

--- docker/docker-4dn-hic/Build_docker.sh
+++ docker/docker-4dn-hic/Build_docker.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-#240402 New docker images
-docker build -t kuzobuta/4dn-hic:v43 .
-
-#230704 Install docker images for bug fix
-#To build docker & add the new_script 
-#docker build -t duplexa/4dn-hic:v43.pre .
-#docker rmi duplexa/4dn-hic:v43
-#docker tag duplexa/4dn-hic:v43.pre duplexa/4dn-hic:v43
-#docker rmi duplexa/4dn-hic:v43.pre
-
+#240708 New docker images
+docker build -t kuzobuta/4dn-hic:v43.1 .
